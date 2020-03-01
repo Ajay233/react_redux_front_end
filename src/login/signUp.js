9f3a7f83bbf997ae2e6e2b5eefe64495dfc667bc
Ajay <ajaymungur@hotmail.com>
2020-03-01 21:47:34 +0000

    Installed axios and implemented SignUp component and POST request to back end API

--- src/login/signUp.js
+++ src/login/signUp.js
@@ -1,17 +1,56 @@
 import React from 'react';
+import axios from 'axios';
 
 class SignUp extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      forename: "",
+      surname: "",
+      email: "",
+      password: "",
+      response: "",
+      error: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  setResponse = (responseData) => {
+    this.setState({ response: responseData })
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:8080/users/auth/signUp',{
+        'forename': this.state.forename,
+        'surname': this.state.surname,
+        'email': this.state.email,
+        'password': this.state.password
+    }).then((response) => {
+      this.setResponse(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   render(){
     return(
       <div>
         Sign up below
-        <form>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
+        <form onSubmit={this.handleSubmit}>
+          <input name="forename" onChange={this.handleChange}></input>
+          <input name="surname" onChange={this.handleChange}></input>
+          <input name="email" onChange={this.handleChange}></input>
+          <input name="password" onChange={this.handleChange}></input>
           <button>Go</button>
         </form>
+        <div>{this.state.response}</div>
       </div>
     );
   }
