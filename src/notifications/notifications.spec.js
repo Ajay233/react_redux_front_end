5e39858e979bbe627fa503eb61fb6bdd51386963
Ajay <ajaymungur@hotmail.com>
2020-05-14 23:19:02 +0100

    Added snapshot tests for the notification component based on redux notificationData state

--- src/notifications/notifications.spec.js
+++ src/notifications/notifications.spec.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import Notification from './notifications'
+import configureStore from 'redux-mock-store'
+import renderer from 'react-test-renderer'
 import { mapStateToProps } from './notifications'
 
+const mockStore = configureStore([]);
 
 describe('mapStateToProps', () => {
   it('can map state to props', ()=> {
@@ -23,10 +28,51 @@ describe('mapStateToProps', () => {
 });
 
 describe('Notification component', () => {
+
   it('can render a notification', () => {
-    // snapshot test
+
+    let store;
+    let component;
+
+    store = mockStore({
+      notificationData: {
+        message: "Test message",
+        type: "success",
+        show: true,
+        timed: true
+      }
+    });
+
+    component = renderer.create(
+      <Provider store={store}>
+        <Notification />
+      </Provider>
+    );
+
+    expect(component).toMatchSnapshot();
+
   })
-  it('can hide a notification after 2 seconds', () => {
-    // snapshot test ?
+
+  it('can hide a notification message when the show property is set to false', () => {
+    let store;
+    let component;
+
+    store = mockStore({
+      notificationData: {
+        message: "",
+        type: "",
+        show: false,
+        timed: true
+      }
+    });
+
+    component = renderer.create(
+      <Provider store={store}>
+        <Notification />
+      </Provider>
+    );
+
+    expect(component).toMatchSnapshot();
   })
+
 });
