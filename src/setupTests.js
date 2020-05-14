dd943948afdd17ef516da4edd0bd66447d19689e
Ajay <ajaymungur@hotmail.com>
2020-05-14 21:37:50 +0100

    Installed enzyme for testing and created the test setup file

---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+//Another property that could be added is:
+// disableLifecycleMethods: true
