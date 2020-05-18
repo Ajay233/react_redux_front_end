91c0430d5b3ef3c88642901fd2c321adb7217c97
Ajay <ajaymungur@hotmail.com>
2020-05-18 01:26:38 +0100

    Added tests for quizStart reducers

---
+++ src/quizStart/reducers/quizStartReducerTest.spec.js
@@ -0,0 +1,73 @@
+import { addAnswer, incrementQuestion, showResults, exitQuiz } from '../actions'
+import { setQuizProgressTracking } from './index'
+
+describe("setQuizProgressTracking", () => {
+  it("should add the selected answer to the snawersPisked state", () => {
+    const initialState = {
+      questionNumber: 0,
+      answersPicked:[],
+      showResults: false
+    }
+
+    const expectedState = {
+      questionNumber: 0,
+      answersPicked:[
+        { id: 1, answerNumber: 1 }
+      ],
+      showResults: false
+    }
+
+    const answer = { id: 1, answerNumber: 1 }
+
+    const newState = setQuizProgressTracking(initialState, addAnswer(answer))
+
+    expect(newState).toEqual(expectedState)
+  })
+
+  it("should increment the questionNumber state", () => {
+    const initialState = {
+      questionNumber: 0,
+      answersPicked:[],
+      showResults: false
+    }
+
+    const expectedState = {
+      questionNumber: 1,
+      answersPicked:[],
+      showResults: false
+    }
+
+    const newState = setQuizProgressTracking(initialState, incrementQuestion(1))
+
+    expect(newState).toEqual(expectedState)
+  })
+
+  it("should set the showResults state to true", () => {
+    const initialState = {
+      questionNumber: 0,
+      answersPicked:[],
+      showResults: false
+    }
+
+    const expectedState = {
+      questionNumber: 0,
+      answersPicked:[],
+      showResults: true
+    }
+
+    const newState = setQuizProgressTracking(initialState, showResults())
+
+    expect(newState).toEqual(expectedState)
+  })
+
+  it("should return the default state if an unrecognised action is passed in", () => {
+    const initialState = {
+      questionNumber: 0,
+      answersPicked:[],
+      showResults: false
+    }
+    const action = { type: "UNRECOGNISED_ACTION"}
+    const newState = setQuizProgressTracking(initialState, action)
+    expect(newState).toEqual(initialState)
+  })
+})
