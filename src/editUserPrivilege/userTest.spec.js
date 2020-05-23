3c07cf0e680a4ff763802339e696d2fddf810986
Ajay <ajaymungur@hotmail.com>
2020-05-24 00:57:33 +0100

    Added test for User component

---
+++ src/editUserPrivilege/userTest.spec.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import User from './user'
+import renderer from 'react-test-renderer'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import history from '../history'
+
+describe("User", () => {
+  it("should render a user's details using the props passed to it", () => {
+    const userResults = {
+      forename: "testForename",
+      surname: "testSurname",
+      email: "testEmail",
+      permission: "USER",
+      verified: true
+    }
+    const component = renderer.create(
+      <User userResults={userResults} />
+    )
+
+    expect(component).toMatchSnapshot()
+  })
+})
