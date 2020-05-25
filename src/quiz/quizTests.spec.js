c9d9a394ee4aeb5784ad15f6f068bdcde5fe52fb
Ajay <ajaymungur@hotmail.com>
2020-05-25 23:11:55 +0100

    Updated import name of file

--- src/quiz/quizTests.spec.js
+++ src/quiz/quizTests.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Quiz, mapStateToProps } from './Quiz'
+import { Quiz, mapStateToProps } from './quiz'
 import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
