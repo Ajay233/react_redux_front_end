ac71add207a10f864b6503d35afa8106de7dc93e
Ajay <ajaymungur@hotmail.com>
2020-08-21 00:43:22 +0100

    Added the content fo the taking a quiz help component

--- src/help/contentComponents/takingQuizzesHelp.js
+++ src/help/contentComponents/takingQuizzesHelp.js
@@ -5,16 +5,51 @@ class TakingQuizzesHelp extends React.Component {
   render(){
     return(
       <div className="componentContainer">
-        <div className="title-large-spaced"></div>
+        <div className="title-large-spaced">Taking a quiz</div>
         <div className="helpContainer">
           <div className="helpSideBar">
           </div>
           <div className="helpContent">
+
+            <div className="helpSectionSpacing">
+              <div className="title-medium-left-alt bold"></div>
+              <div className="">
+                To take a quiz you will first need to find a quiz using either the quiz search or browse all quizzes pages.
+              </div>
+              <ol>
+                <li>
+                  In addition to the quiz options for each quiz result, where a quiz has been marked as ready,
+                  there is an option to take the quiz.  Clicking on this option will start the quiz.
+                </li>
+                <li>
+                  When clicking on this option a pop u will ask you to confirm if you are sure you want to start the quiz.
+                  This prevents the quiz from being started by accident.
+                </li>
+                <li>
+                  After confirming you want to start the quiz, you will be taken to that quiz’s Q&A page where you will
+                  start the quiz.
+                </li>
+                <div className="">
+                  The current question you are being asked is displayed on the left.
+                </div>
+                <div className="">
+                  You will have a list of potential answers to pick from on the right.
+                </div>
+                <li>Select an answer and then click ‘next question’.</li>
+                <li>When you answer the last question, you will be given your score.</li>
+                <li>
+                  Once you have noted your score, click finish to exit back to the home page where you can continue
+                  using the app.
+                </li>
+              </ol>
+            </div>
+
             <div className="helpSectionSpacing">
               <div className="title-medium-left-alt bold"></div>
               <div className="">
               </div>
             </div>
+
           </div>
         </div>
       </div>
