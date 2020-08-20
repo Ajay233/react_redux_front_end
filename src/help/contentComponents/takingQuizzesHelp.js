abb28fbdbe61f8d75c168561476b69132fdadb28
Ajay <ajaymungur@hotmail.com>
2020-08-20 21:27:16 +0100

    Created components for each help page and added template structure to each

---
+++ src/help/contentComponents/takingQuizzesHelp.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+class TakingQuizzesHelp extends React.Component {
+  render(){
+    return(
+      <div className="componentContainer">
+        <div className="title-large-spaced"></div>
+        <div className="helpContainer">
+          <div className="helpSideBar">
+          </div>
+          <div className="helpContent">
+            <div className="helpSectionSpacing">
+              <div className="title-medium-left-alt bold"></div>
+              <div className="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TakingQuizzesHelp
