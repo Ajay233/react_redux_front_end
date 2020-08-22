5d62fe798bc2a3d7e496efe0cc226d3c32cd974f
Ajay <ajaymungur@hotmail.com>
2020-08-22 15:41:15 +0100

    Added screneshots to the edit privilege help component

--- src/help/contentComponents/editUserPrivilegeHelp.js
+++ src/help/contentComponents/editUserPrivilegeHelp.js
@@ -21,18 +21,36 @@ class EditUserPrivilegeHelp extends React.Component {
             </div>
             <div className="helpSectionSpacing">
               <div className="title-medium-left-alt bold"></div>
-              <div className="">
+              <div className="spacedParagraph">
                 Next you will need to enter the user’s email address and then click the search button.  You will
                 have received a request from a user to upgrade their access privilege which will contain the user’s
                 email address.
               </div>
+              <img src={require(`../../public/help/editUserPrivileges/1.png`)} className="screenshot" alt=""/>
+              <img src={require(`../../public/help/editUserPrivileges/2.png`)} className="screenshot" alt=""/>
+              <div className="warning spacedParagraph">
+                <b><i className="fas fa-exclamation-triangle"></i> Note:</b> You won’t be allowed to perform the
+                search without first entering an email address
+              </div>
+              <img src={require(`../../public/help/editUserPrivileges/3.png`)} className="screenshot" alt=""/>
             </div>
             <div className="helpSectionSpacing">
               <div className="title-medium-left-alt bold"></div>
-              <div className="">
-                Once found, the user’s account details will be displayed below the search bar.  You will also be
-                provided with a drop down where you can select from the list of access privileges you which to set
-                the user’s access privilege to.  Select an option and then click save.
+              <div className="spacedParagraph">
+                Once found, the user’s account details will be displayed below the search bar.
+              </div>
+              <img src={require(`../../public/help/editUserPrivileges/4.png`)} className="screenshot" alt=""/>
+              <div className="spacedParagraph">
+                You will also be provided with a drop down where you can select from the list of access privileges
+                you which to set the user’s access privilege to.
+              </div>
+              <img src={require(`../../public/help/editUserPrivileges/5.png`)} className="screenshot" alt=""/>
+              <div className="spacedParagraph">
+                Select an option and then click save.
+              </div>
+              <img src={require(`../../public/help/editUserPrivileges/6.png`)} className="screenshot" alt=""/>
+              <div className="spacedParagraph">
+                A notification will be displayed confirming the successful change to the selected user's privilege level.
               </div>
             </div>
           </div>
