3e48bf0305c51461c2aef0d86057ab034b60108e
Ajay <ajaymungur@hotmail.com>
2020-08-28 01:18:38 +0100

    Added screenshots for the find quizzes help component

--- src/help/contentComponents/findingQuizzesHelp.js
+++ src/help/contentComponents/findingQuizzesHelp.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import SideBar from './sideBar'
+import ImageModal from '../../modal/imageModal'
+import { showModal, hideModal } from '../../modal/actions'
+import img1 from '../../public/help/FindingQuizzes/1.png'
+import img2 from '../../public/help/FindingQuizzes/2-anotated.png'
+import img3 from '../../public/help/FindingQuizzes/3.png'
+import img4 from '../../public/help/FindingQuizzes/4-anotated.png'
 
 class FindingQuizzesHelp extends React.Component {
+
+  componentDidMount(){
+    document.documentElement.scrollTop = 0;
+  }
+
   render(){
+    const { modalState, hideModal, showModal } = this.props
     return(
       <div className="componentContainer-alt">
-
+          <ImageModal
+            show={modalState.showModal}
+            onCancel={hideModal}
+            imgPath={modalState.imgPath}
+          />
           <div className="helpSideBar">
             <SideBar />
           </div>
@@ -20,6 +37,12 @@ class FindingQuizzesHelp extends React.Component {
               <ol>
                 <li>Click on the quiz search option in the nav bar drop down menu</li>
                 <li>This will take you to the quiz search page where you can perform a search by either name or category.</li>
+                <img
+                  src={require(`../../public/help/FindingQuizzes/1.png`)}
+                  className="screenshot"
+                  alt=""
+                  onClick={() => {showModal(img1)}}
+                />
               </ol>
             </div>
 
@@ -31,9 +54,21 @@ class FindingQuizzesHelp extends React.Component {
                 <li>
                   To search by name you will need to know the name of the quiz.  Enter it in the name field and click search.
                 </li>
+                <img
+                  src={require(`../../public/help/FindingQuizzes/3.png`)}
+                  className="screenshot"
+                  alt=""
+                  onClick={() => {showModal(img3)}}
+                />
                 <li>
                   To search by category, simply select a category form the drop down list and click search.
                 </li>
+                <img
+                  src={require(`../../public/help/FindingQuizzes/2-anotated.png`)}
+                  className="screenshot"
+                  alt=""
+                  onClick={() => {showModal(img2)}}
+                />
                 <li>
                   The results will render below the search sections.  Each quiz will be displayed, providing the name,
                   description and options.
@@ -52,6 +87,12 @@ class FindingQuizzesHelp extends React.Component {
               <div className="">
                 Another option is to use the Browse quizzes page which will let you browse through all available quizzes per category.
               </div>
+              <img
+                src={require(`../../public/help/FindingQuizzes/4-anotated.png`)}
+                className="screenshot"
+                alt=""
+                onClick={() => {showModal(img4)}}
+              />
             </div>
           </div>
 
@@ -60,4 +101,10 @@ class FindingQuizzesHelp extends React.Component {
   }
 }
 
-export default FindingQuizzesHelp
+const mapStateToProps = (state) => {
+  return {
+    modalState: state.modalState
+  }
+}
+
+export default connect(mapStateToProps, { showModal, hideModal })(FindingQuizzesHelp)
