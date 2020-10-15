44032605ff59db32b7b119985347ccb593fc5697
Ajay <ajaymungur@hotmail.com>
2020-10-15 03:03:07 +0100

    Index was unable to find app during deploy so have amended the import as a possible fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 
-import App from './app.js'
+import App from './app'
 // import reducers from './reducers'
 import { rootReducer } from './reducers'
 
